6e13e40a2cad47bd488c87e1e3aadee201a6fce2
neldev <neldev@NelDev>
2015-05-13 13:45:41 -0300

    #--- COMMIT --- Pequenas modificações # 1) ConnectionFactory.java: Passa a receber conexão com o banco de dados na núvem. 2) Students.xhtml: Botão "Novo Aluno" estava faltando o component "form'. 3) pom.xml: Mudança de versão do Drive do MySql de "5.1.6" para "5.1.34".

--- src/main/java/br/com/coachcrossfit/database/ConnectionFactory.java
+++ src/main/java/br/com/coachcrossfit/database/ConnectionFactory.java
@@ -8,13 +8,25 @@ public class ConnectionFactory {
 	public Connection getConnection(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost/coach_crossfit";
-			return DriverManager.getConnection(url, "root", "123456");			
+			String url = "jdbc:mysql://coachcrossfit.cbvshgri1s60.sa-east-1.rds.amazonaws.com:3306/coachcrossfit?user=managercoach&password=aL69esaBataD30#";
+			return DriverManager.getConnection(url);	 		
 		}
 		catch(Exception e){
 			throw new RuntimeException("Falha na conexão com o banco de dados \n "
 					+ "O Sistema apresenta a seguinte informação: "+e.getMessage());
 		}		
 	}
-			
+	
+//	public Connection getConnection(){
+//		try{
+//			Class.forName("com.mysql.jdbc.Driver");
+//			String url = "jdbc:mysql://localhost/coachcrossfit";
+//			return DriverManager.getConnection(url, "root", "123456");			
+//		}
+//		catch(Exception e){
+//			throw new RuntimeException("Falha na conexão com o banco de dados \n "
+//					+ "O Sistema apresenta a seguinte informação: "+e.getMessage());
+//		}		
+//	}
+ 			
 }
